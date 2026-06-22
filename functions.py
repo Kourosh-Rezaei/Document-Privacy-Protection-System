e97bb3e83ce752a923f75c469b192b23f1b49dd3
Kourosh Rezaei <kouroshrezaei.tech@gmail.com>
2026-06-22 13:06:25 +0330

    Implement prepare_data function

--- functions.py
+++ functions.py
@@ -1,15 +1,37 @@
 from docx import Document
+import re
 
 
-def read_docx(file_path):
+def read_docx(file_path: str) -> str:
+    """
+    Extract the content of the DOCX file
+    """
     try:
         doc = Document(file_path)
         full_text = []
         for para in doc.paragraphs:
-            print(para)
             full_text.append(para.text)
 
         return '\n'.join(full_text)
 
     except Exception as e:
         return f"error: {e}"
+
+
+def prepare_data(text: str) -> str:
+    """
+    Find all phone numbers and names and at them to a list of dictionaries
+    """
+    data = []
+
+    pattern = r'([A-Za-z\s]+)\s+(\+\d{1,3}\s?\d{3}\s?\d{3}\s?\d{4})'
+
+    matches = re.findall(pattern, text)
+
+    for name, phone in matches:
+        data.append({
+            "name": name.strip(),
+            "Phone Number": phone.strip()
+        })
+
+    return data
